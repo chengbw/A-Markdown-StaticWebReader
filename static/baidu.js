9fe47303c5f7c2375381c958246a3675e52e76ed
chengbw <log.mail@qq.com>
2019-01-19 15:38:06 +0800

    add baidu link js

--- static/baidu.js
+++ static/baidu.js
@@ -1,3 +1,4 @@
+//baidu status
 var _hmt = _hmt || [];
 (function () {
     var hm = document.createElement("script");
@@ -6,3 +7,17 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(hm, s);
 })();
 
+
+//baidu link
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
