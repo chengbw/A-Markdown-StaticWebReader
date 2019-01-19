7c0380fc676bf95cad6dbb3d4cdfd9e25b7c7cd9
chengbw <log.mail@qq.com>
2019-01-19 13:42:39 +0800

    add baidu stats js

---
+++ static/baidu.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a17a0fef04ab22d771165e165756dfdd";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
